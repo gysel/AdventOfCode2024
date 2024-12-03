9079864744e1a6b77c6358553f81522341bb2d6c
Michael Gysel <michael.gysel@unblu.com>
2024-12-03 06:40:31 +0100

    Use immutable fold instead of state

--- src/main/kotlin/Day03.kt
+++ src/main/kotlin/Day03.kt
@@ -3,32 +3,29 @@ fun main() {
 
     solve("Part 1", 175015740L) {
         val regex = Regex("mul\\((\\d+),(\\d+)\\)")
-        regex.findAll(data).sumOf { match ->
-            match.groupValues
-                .drop(1)
-                .map { it.toLong() }
-                .reduce(Long::times)
-        }
+        regex.findAll(data).sumOf(::calculateProduct)
     }
 
     solve("Part 2", 112272912L) {
         val regex = Regex("mul\\((\\d+),(\\d+)\\)|do\\(\\)|don't\\(\\)")
-        var result = 0L
-        var enabled = true
-        regex.findAll(data).forEach { match ->
-            when (match.value) {
-                "do()" -> enabled = true
-                "don't()" -> enabled = false
-                else -> {
-                    if (enabled) {
-                        result += match.groupValues
-                            .drop(1)
-                            .map { it.toLong() }
-                            .reduce(Long::times)
+        regex.findAll(data)
+            .fold(0L to true) { state, match: MatchResult ->
+                var (sum, enabled) = state
+                when (match.value) {
+                    "do()" -> enabled = true
+                    "don't()" -> enabled = false
+                    else -> {
+                        if (enabled) {
+                            sum += calculateProduct(match)
+                        }
                     }
                 }
-            }
-        }
-        result
+                sum to enabled
+            }.first
     }
 }
+
+private fun calculateProduct(match: MatchResult) = match.groupValues
+    .drop(1)
+    .map { it.toLong() }
+    .reduce(Long::times)
