2f97c37553093845d70b052cc8ea21abb186d83c
Michael Gysel <michael.gysel@unblu.com>
2024-12-12 20:27:57 +0100

    Day 12, working but ugly solution

--- src/main/kotlin/Day12.kt
+++ src/main/kotlin/Day12.kt
@@ -1,9 +1,9 @@
+import Direction.LEFT
 import java.util.*
 
 fun main() {
 
-    solve("Example 1", 1930) {
-        val sampleInput = """
+    val sampleInput = """
             RRRRIICCFF
             RRRRIICCCF
             VVRRRCCFFF
@@ -15,19 +15,23 @@ fun main() {
             MIIISIJEEE
             MMMISSJEEE
         """.trimIndent().lines().let(::parseCharacterGrid)
-
-        val areas = findAreas(sampleInput)
-        areas.sumOf { calculateFencePrice(it, sampleInput) }
+    solve("Example part 1", 1930) {
+        findAreas(sampleInput).sumOf { calculateFencePrice(it, sampleInput) }
     }
 
     val input = InputData.readLines("day12.txt")
         .let(::parseCharacterGrid)
-    solve("Part 1", null) {
-        val areas = findAreas(input)
-        println("Found ${areas.size} areas")
-        areas.sumOf { calculateFencePrice(it, input) }
+    solve("Part 1", 1477762) {
+        findAreas(input).sumOf { calculateFencePrice(it, input) }
     }
-    solve("Part 2", null) {
+
+    solve("Example part 2", 1206) {
+        findAreas(sampleInput).sumOf { calculateFencePrice(it, sampleInput, true) }
+    }
+
+    solve("Part 2", 923480) {
+        val areas = findAreas(input)
+        areas.sumOf { calculateFencePrice(it, input, true) }
     }
 
 }
@@ -39,29 +43,63 @@ fun findAreas(grid: Map<Coordinates, Char>): List<List<Coordinates>> {
     do {
         val c = toProcess.removeFirst()
         val plant = grid[c]!!
-        val potentialNeighbours = grid.filter { it.value == plant }.keys
-        val neighbours = mutableListOf<Coordinates>()
-        neighbours.add(c)
-        val area = mutableSetOf<Coordinates>()
-        do {
-            val x = neighbours.removeFirst()
-            neighbours.addAll(
-                x.neighbours()
-                    .filter { it in potentialNeighbours }
-                    .filter { it !in area }
-                    .filter { grid[it] == plant })
-            area.add(x)
-        } while (neighbours.isNotEmpty())
-        toProcess.removeAll(area)
+        // println("Processing area of $plant (${toProcess.size} areas remaining to process)")
+        val areasWithSamePlant = grid.filter { it.value == plant }.keys.toMutableSet()
+        areasWithSamePlant.remove(c)
+        val area = mutableSetOf<Coordinates>(c)
+        var currentAreas = listOf(c)
+
+        while (currentAreas.isNotEmpty()) {
+            currentAreas = currentAreas.flatMap { it.neighbours() }
+                .distinct()
+                .filter { it !in area }
+                .filter { it in areasWithSamePlant }
+                .also(area::addAll)
+        }
+
         result.add(area.toList())
+        toProcess.removeAll(area)
     } while (toProcess.isNotEmpty())
     return result.toList()
 }
 
-fun calculateFencePrice(area: List<Coordinates>, grid: Map<Coordinates, Char>): Long {
-    val areaPlant = grid[area.first()]
-    val perimeter: Long = area.sumOf { coordinate ->
-        coordinate.neighbours().count { grid[it] != areaPlant }.toLong()
+fun calculateFencePrice(area: List<Coordinates>, grid: Map<Coordinates, Char>, discount: Boolean = false): Long {
+    val fences: List<Pair<Coordinates, Direction>> = area.flatMap { c ->
+        Direction.entries.mapNotNull { direction ->
+            val neighbour = c.moveTo(direction)
+            if (neighbour !in area) {
+                c to direction
+            } else null
+        }
     }
-    return perimeter * area.size
+    return area.size.toLong() * if (discount) {
+        //calculate number of sides
+        fences.groupBy { it.second }.map { (direction: Direction, fences: List<Pair<Coordinates, Direction>>) ->
+            val sideCount = fences
+                .groupBy { it.first.getRelevantDimension(direction) }
+                .map { (_, lineCandidates) -> lineCandidates.map { it.first.getRelevantDimension(direction.rotate()) } }
+                .sumOf(::countGaps)
+            sideCount
+        }.sum()
+    } else {
+        fences.size
+    }
+}
+
+fun countGaps(numbers: List<Int>): Int {
+    var sides = 1
+    numbers.sorted().let { sorted ->
+        var current = sorted.first()
+        sorted.drop(1).forEach { i ->
+            if (i > current + 1) {
+                sides++
+            }
+            current = i
+        }
+    }
+    return sides
+}
+
+private fun Coordinates.getRelevantDimension(direction: Direction): Int {
+    return if (direction == LEFT || direction == Direction.RIGHT) this.x else this.y
 }
